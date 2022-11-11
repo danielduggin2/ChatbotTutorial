d34de0ed0c35a64bdfc74fcbb4a6380479c3a4c1
Daniel Duggin <46377670+danielduggin2@users.noreply.github.com>
2022-11-11 12:23:41 -0500

    Initial Commit

---
+++ ChatbotTutorial/ChatbotTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatbotTutorialApp.swift
+//  ChatbotTutorial
+//
+//  Created by Daniel Wade Duggin on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatbotTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
